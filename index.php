a2f83945ee3fdc4bee95359e3e241e12f2e3bdde
joaovitora18 <jvacs2019@gmail.com>
2023-01-24 11:24:07 -0300

    cabeçalho montado

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Document</title>
+</head>
+<body class="fundo">
+    <!-- inclusão do cabeçalho -->
+    <?php include 'header.html'?>
+    <!-- começo do site -->
+<div class="container bg-transparent produtos">
+    <div class="thumbnail produto">
+        <div>
+            <img src="images/anel.png" class="imgprod" alt="">
+        </div>
+        <div class="informacoes">
+            <p class="text-left">
+                Lorem ipsum dolor sit amet
+            </p>
+            <p class="precos">
+                <button class="btn" role="button" style="cursor:default ;" disabled>
+                    150,20
+                </button>
+                <br>
+                <a href="">saiba mais...</a>
+            </p>
+            
+
+        </div>
+    </div>
+    <div class="thumbnail produto">
+        <div>
+            <img src="images/anel.png" class="imgprod" alt="">
+        </div>
+        <div class="informacoes">
+            <p class="text-left">
+                Lorem ipsum dolor sit amet
+            </p>
+            <p class="precos">
+                <button class="btn" role="button" style="cursor:default ;" disabled>
+                    150,20
+                </button>
+                <br>
+                <a href="">saiba mais...</a>
+            </p>
+            
+
+        </div>
+    </div>
+    <div class="thumbnail produto">
+        <div>
+            <img src="images/anel.png" class="imgprod" alt="">
+        </div>
+        <div class="informacoes">
+            <p class="text-left">
+                Lorem ipsum dolor sit amet
+            </p>
+            <p class="precos">
+                <button class="btn" role="button" style="cursor:default ;" disabled>
+                    150,20
+                </button>
+                <br>
+                <a href="">saiba mais...</a>
+            </p>
+            
+
+        </div>
+    </div>
+    <div class="thumbnail produto">
+        <div>
+            <img src="images/anel.png" class="imgprod" alt="">
+        </div>
+        <div class="informacoes">
+            <p class="text-left">
+                Lorem ipsum dolor sit amet
+            </p>
+            <p class="precos">
+                <button class="btn" role="button" style="cursor:default ;" disabled>
+                    150,20
+                </button>
+                <br>
+                <a href="">saiba mais...</a>
+            </p>
+            
+
+        </div>
+    </div>
+    
+</div>
+    
+</body>
+</html>
